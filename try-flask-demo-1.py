387241f4ef01868c755f2cbade9a25c04827d4d4
Chris Chabalko <chris45419@gmail.com>
2015-09-06 21:17:39 -0400

    gff

---
+++ try-flask-demo-1.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    print(sys.version)
+    print('this file is being read')
+    return sys.version
