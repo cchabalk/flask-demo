cb077384852660060ffa274299883dcdda896a70
Chris Chabalko <chris45419@gmail.com>
2015-09-06 22:13:33 -0400

    added everything

--- try-flask-demo-2.py
+++ try-flask-demo-2.py
@@ -5,9 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import views
+
 #app.run()
 
-@app.route('/')
+#@app.route('/')
 def hello():
     print(sys.version)
     print('this file is being read')
