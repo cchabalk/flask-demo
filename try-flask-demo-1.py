f75840a0a61c425c9233dedecb8f9c5be07078af
Chris Chabalko <chris45419@gmail.com>
2015-09-06 21:25:08 -0400

    h

--- try-flask-demo-1.py
+++ try-flask-demo-1.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 def hello():
     print(sys.version)
     print('this file is being read')
-    return print('this file is being read')+sys.version
+    return print('this file is being read')
